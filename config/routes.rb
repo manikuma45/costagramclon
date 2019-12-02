13fd41fca2f3feed1c337410706c851bc36f65ae
manish kumar <manikuma0405@gmail.com>
2019-12-02 13:50:31 +0900

    ログインフォームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
   resources :messages
-  resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
