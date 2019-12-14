d6fc3182833449980aa1c0a6282fc193dec1ada2
manish kumar <manikuma0405@gmail.com>
2019-12-14 11:59:07 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
 	mount_uploader :picture, PictureUploader
 	has_many :messages
 	validates :name,  presence: true, length: { maximum: 30 }
-	validates :email, presence: true, length: { maximum: 255 }
+	validates :email, presence: true, length: { maximum: 255 },
+					  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
    before_validation { email.downcase! }
    has_secure_password
    validates :password, presence: true, length: { minimum: 6 }
